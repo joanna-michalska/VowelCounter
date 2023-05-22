7466fbf0dee857254c34efad283840c9eff88e8c
Joanna Michalska <asiamichalska9@gmail.com>
2023-05-22 19:04:24 +0200

    Added a separate service, added reading user input

---
+++ VowelCounter/VowelService.cs
@@ -0,0 +1,20 @@
+﻿namespace VowelCounter
+{
+    internal class VowelService
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            List<string> vowels = new List<string> { "a", "e", "i", "o", "u" };
+            foreach (char c in str)
+            {
+                if (vowels.Contains(c.ToString()))
+                {
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
