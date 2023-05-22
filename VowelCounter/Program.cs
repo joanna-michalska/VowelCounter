efbcb986ab4f69441e3ca3fefcb1aa6dd86102fe
Joanna Michalska <asiamichalska9@gmail.com>
2023-05-22 19:08:27 +0200

    Added check for empty or null input string

--- VowelCounter/Program.cs
+++ VowelCounter/Program.cs
@@ -7,6 +7,12 @@
             Console.WriteLine("Enter your text: ");
             var userInput = Console.ReadLine();
 
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("No text provided. Re-run the program!");
+                return;
+            }
+
             Console.WriteLine(VowelService.GetVowelCount(userInput));
         }
     }
